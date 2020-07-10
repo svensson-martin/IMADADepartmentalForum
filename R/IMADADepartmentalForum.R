cb391cefeec058d05451abc53cbe97cebcbbcd36
martinsvensson70 <svensson@imada.sdu.dk>
2020-07-10 11:02:18 +0200

    Finalizing

--- R/IMADADepartmentalForum.R
+++ R/IMADADepartmentalForum.R
@@ -1,7 +1,10 @@
-#' IMADADepartmentalForum: A template pdf presentation for IMADA Departmental Fora
+#' IMADADepartmentalForum: A template presentation for IMADA Departmental Fora
 #'
 #' This template creates an R-markdown file and a Latex preamble.
 #'
+#' @section Intstallation:
+#' Call devtools::install_github("svensson-martin/IMADADepartmentalForum") which
+#' will install it as an RStudio template
 #' @docType package
 #' @name IMADADepartmentalForum
 NULL
