b50b029847c0e8e85527f44036ac647f18c88715
martinsvensson70 <svensson@imada.sdu.dk>
2020-07-10 10:26:00 +0200

    First commit

---
+++ R/IMADADepartmentalForum.R
@@ -0,0 +1,8 @@
+#' IMADADepartmentalForum: A template pdf presentation for IMADA Departmental Fora
+#'
+#' This template creates an R-markdown file and a Latex preamble.
+#'
+#' @docType package
+#' @name IMADADepartmentalForum
+NULL
+
